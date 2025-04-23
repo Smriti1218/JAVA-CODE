1a77aaca2cbc18b997ef2406f10d3b8fe302bca3
Smriti1218 <smritithakur030@gmail.com>
2025-04-23 15:10:04 +0530

    number of nodes on a specific level

--- Tree.java
+++ Tree.java
@@ -1,31 +1,30 @@
-// Height of the tree
-public class Tree{
-    static class TreeNode{
-        int data;
-        TreeNode left;
-        TreeNode right;
-        //make a constructor
-        TreeNode(int value){
-            data= value;
-            left=right=null;
-        }
-    }
-    public static int height(TreeNode root){
-        if(root==null){
-            return 0;
-        }
-        int leftHeight=height(root.left);
-        int rightHeight=height(root.right);
-        return Math.max(leftHeight, rightHeight)+1;
+// // Height of the tree
+// public class Tree{
+//     static class TreeNode{
+//         int data;
+//         TreeNode left;
+//         TreeNode right;
+//         //make a constructor
+//         TreeNode(int value){
+//             data= value;
+//             left=right=null;
+//         }
+//     }
+//     public static int height(TreeNode root){
+//         if(root==null){
+//             return 0;
+//         }
+//         int leftHeight=height(root.left);
+//         int rightHeight=height(root.right);
+//         return Math.max(leftHeight, rightHeight)+1;
 
-    }
-    public static void main(String[] args){
-        TreeNode root = new TreeNode(10);
-        root.left=new TreeNode(20);
-        root.right=new TreeNode(30);
-        root.left.left=new TreeNode(40);
-        root.left.right=new TreeNode(50);
-        System.out.println("Height of the tree is "+height(root));
-    }
-}
-
+//     }
+//     public static void main(String[] args){
+//         TreeNode root = new TreeNode(10);
+//         root.left=new TreeNode(20);
+//         root.right=new TreeNode(30);
+//         root.left.left=new TreeNode(40);
+//         root.left.right=new TreeNode(50);
+//         System.out.println("Height of the tree is "+height(root));
+//     }
+// }
